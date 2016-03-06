ae20b956a75507b3e5c59266b853e20c023fd62a
irinanovoselskaya <irina.novoselskaya@teamdev.com>
2016-03-06 17:01:04 +0200

    Refactoring js, css

--- js/spine.js
+++ js/spine.js
@@ -2,98 +2,69 @@
  * Spine Theme
  */
 
-// Preloader
-jQuery(window).load(function() {
-  // will first fade out the loading animation
-  jQuery("#status").fadeOut();
-  // will fade out the whole DIV that covers the website.
-  jQuery("#preloader").delay(1000).fadeOut("slow");
-})
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-  if ($(".navbar").offset().top > 50) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
-  } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
-  }
-});
 
 $(document).ready(function(){
 
-  if ( !$('body').hasClass('blog') ) {
-    var scrollBarWidth = getScrollBarWidth();
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
 
-    $('.navbar-fixed-top').width($('body').width()
-        - scrollBarWidth
-        - parseFloat($('.navbar-fixed-top').css('padding-left'))
-        - parseFloat($('.navbar-fixed-top').css('padding-right')));
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+    wow.init();
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
   }
 
-  if ($('.parallax').scrollTop() > 100) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse animated");
-  } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse animated");
+  if ( !$('body').hasClass('blog') ) {
+    var scrollBarWidth = getScrollBarWidth();
+    calculateNaviWidth(scrollBarWidth);
   }
 
   $('.parallax').scroll(function(){
-    if ($('.parallax').scrollTop() > 100) {
-      $(".navbar-fixed-top").addClass("top-nav-collapse animated");
-    } else {
-      $(".navbar-fixed-top").removeClass("top-nav-collapse animated");
-    }
+    anmatedNaviMain();
   });
 
   $(window).resize(function(){
     if ( !$('body').hasClass('blog') ) {
-      $('.navbar-fixed-top').width($('body').width()
-          - scrollBarWidth
-          - parseFloat($('.navbar-fixed-top').css('padding-left'))
-          - parseFloat($('.navbar-fixed-top').css('padding-right')));
+      calculateNaviWidth(scrollBarWidth);
     }
   });
-});
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-  $('a.page-scroll').bind('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 64)
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-  target: '.navbar-fixed-top',
-  offset: 65
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  $('.navbar-toggle:visible').click();
-});
-
-// HTML5 Placeholder
-$(function() {
-  $('input, textarea').placeholder();
-});
-
-// Load WOW.js on non-touch devices
-var isPhoneDevice = "ontouchstart" in document.documentElement;
-$(document).ready(function() {
+  // Load WOW.js on non-touch devices
+  var isPhoneDevice = "ontouchstart" in document.documentElement;
   if (isPhoneDevice) {
-    //mobile
   } else {
-    //desktop
     wow = new WOW({
       offset: 50
     });
     wow.init();
   }
 });
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  $('.navbar-toggle:visible').click();
+});
+
+function anmatedNaviMain() {
+  if ($('.parallax').scrollTop() > 100) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+}
+
+function calculateNaviWidth(scrollBarWidth) {
+  $('.navbar-fixed-top').width($('body').width()
+      - scrollBarWidth
+      - parseFloat($('.navbar-fixed-top').css('padding-left'))
+      - parseFloat($('.navbar-fixed-top').css('padding-right')));
+}
 
 function getScrollBarWidth() {
   var inner = document.createElement('p');
