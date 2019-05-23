eec86bf205c3981fc4248c931896e8942094296e
juliaevseeva <julia.evseeva@teamdev.com>
2019-05-23 12:43:14 +0300

    Make report information collapsible

--- js/os-licenses.js
+++ js/os-licenses.js
@@ -26,8 +26,12 @@ $(
         function makeCollapsibleTitle(mdDestinationID) {
             var h1Elements = mdDestinationID.find("h1");
             var h2Elements = mdDestinationID.find("h2");
+            var linkElements = mdDestinationID.find("a");
 
             h1Elements.addClass("dependencies-title");
+
+            linkElements.addClass("external");
+            linkElements.attr("target", "_blank");
 
             h2Elements.each(function(index, element) {
                 const titleID = this.id + "_1";
@@ -36,9 +40,22 @@ $(
                 $(element).attr("data-toggle", "collapse");
                 $(element).next("ol").addClass("dependencies-container collapse");
                 $(element).next("ol").attr("id", titleID);
-                $(element).next("ol").next("p").css("display", "none");
-                $(element).next("ol").find("a").addClass("external");
-                $(element).next("ol").find("a").attr("target", "_blank");
+
+                $(element).next("ol").next("p").addClass("report-info collapse");
+                $(element).next("ol").next("p").attr("id", titleID + "_p");
+            });
+
+            $("<h2 class='report-info-title collapse-link collapsed'>Report info</h2>").insertBefore(".report-info");
+            makeReportInfoCollapsible(mdDestinationID);
+        }
+
+        function makeReportInfoCollapsible(mdDestinationID) {
+            var reportInfoContent = mdDestinationID.find(".report-info");
+
+            reportInfoContent.each(function (index, element) {
+                const reportInfoID = this.id;
+                $(element).prev(".report-info-title").attr("href", "#" + reportInfoID);
+                $(element).prev(".report-info-title").attr("data-toggle", "collapse");
             });
         }
     }
