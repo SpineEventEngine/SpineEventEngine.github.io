12cc3e1e917a8bdca9fd4b993d646a2abedd460f
juliaevseeva <julia.evseeva@teamdev.com>
2019-05-23 16:37:09 +0300

    Add JSDocs and variables for the repeating elements

--- js/os-licenses.js
+++ js/os-licenses.js
@@ -1,9 +1,20 @@
+/**
+ * This is a JavaScript file which loads `license-report` md-files from the external repositories.
+ *
+ * Please see `/os-licenses/index.html` for usage.
+ */
+
 $(
     function() {
         var converter = new showdown.Converter();
         const loadedAttr = "loaded";
         const repoAttr = "repo";
 
+        /**
+         * Loads `license-report` file from the `SpineEventEngine/base` repository.
+         *
+         * <p>Executes by clicking on the corresponding link.
+         */
         $("#load-md-btn-base").click(function () {
             var clickedElement = $(this);
             var mdDestinationID = $("#md-destination-base");
@@ -23,6 +34,11 @@ $(
             }
         });
 
+        /**
+         * Makes a `license-report` content collapsible.
+         *
+         * @param mdDestinationID id of the `div` with a markdown content
+         */
         function makeCollapsibleTitle(mdDestinationID) {
             var h1Elements = mdDestinationID.find("h1");
             var h2Elements = mdDestinationID.find("h2");
@@ -30,37 +46,59 @@ $(
 
             h1Elements.addClass("dependencies-title");
 
+            /**
+             * Removes `Dependencies of` words from the title.
+             */
             h1Elements.each(function() {
                 var text = $(this).text();
                 $(this).text(text.replace('Dependencies of', ''));
             });
 
+            /**
+             * Makes all markdown links external.
+             */
             linkElements.addClass("external");
             linkElements.attr("target", "_blank");
 
+            /**
+             * Adds required classes and attributes to make titles and content collapsible.
+             */
             h2Elements.each(function(index, element) {
                 const titleID = this.id + "_1";
+                const collapsibleContent = $(element).next("ol");
+                const reportInfoContent = collapsibleContent.next("p");
+
                 $(element).addClass("collapse-link collapsed");
                 $(element).attr("href", "#" + titleID);
                 $(element).attr("data-toggle", "collapse");
-                $(element).next("ol").addClass("dependencies-container collapse");
-                $(element).next("ol").attr("id", titleID);
 
-                $(element).next("ol").next("p").addClass("report-info collapse");
-                $(element).next("ol").next("p").attr("id", titleID + "_p");
+                collapsibleContent.addClass("dependencies-container collapse");
+                collapsibleContent.attr("id", titleID);
+
+                reportInfoContent.addClass("report-info collapse");
+                reportInfoContent.attr("id", titleID + "_p");
             });
 
+            /**
+             * Inserts a new collapsible title for the paragraph with the report information.
+             */
             $("<h2 class='report-info-title collapse-link collapsed'>Report info</h2>").insertBefore(".report-info");
             makeReportInfoCollapsible(mdDestinationID);
         }
 
+        /**
+         * Makes report information content collapsible.
+         * @param mdDestinationID id of the `div` with a markdown content
+         */
         function makeReportInfoCollapsible(mdDestinationID) {
             var reportInfoContent = mdDestinationID.find(".report-info");
 
             reportInfoContent.each(function (index, element) {
                 const reportInfoID = this.id;
-                $(element).prev(".report-info-title").attr("href", "#" + reportInfoID);
-                $(element).prev(".report-info-title").attr("data-toggle", "collapse");
+                const reportInfoTitle = $(element).prev(".report-info-title");
+
+                reportInfoTitle.attr("href", "#" + reportInfoID);
+                reportInfoTitle.attr("data-toggle", "collapse");
             });
         }
     }
