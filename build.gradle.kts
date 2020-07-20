d274be3f9cf1d0823a385638faca716e1e2d663e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2020-07-20 17:12:29 +0300

    Fix script paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,14 +22,14 @@
  * Builds and runs the site locally.
  */
 task<Exec>("runSite") {
-    commandLine("../_script/jekyll-serve")
+    commandLine("./_script/jekyll-serve")
 }
 
 /**
  * Builds the site without starting the server.
  */
 task<Exec>("buildSite") {
-    commandLine("../_script/jekyll-build")
+    commandLine("./_script/jekyll-build")
 }
 
 /**
