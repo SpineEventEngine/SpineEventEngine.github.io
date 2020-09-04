99c17bad151dbcf0f8edf4eaec6a585018a22488
juliaevseeva <julia.evseeva@teamdev.com>
2020-09-04 18:36:16 +0300

    Add Development Support Agreement page
    The page contains custom ordered list.

--- js/common.js
+++ js/common.js
@@ -13,11 +13,13 @@ const topOffset = 12; // Offset from the `header` navigation
 const scrollToOffset = initialHeadHeight + topOffset;
 const $searchField = $('#search-field');
 const mobileSearchOpenedClass = 'mobile-search-opened';
+const $customOrderedList = $('.custom-ordered-list');
 
 /** Pages */
 const isFaqPage = $body.is('.faq');
 const isDocsPage = $body.is('.docs');
 const isPromoPage = $body.is('.promo-page');
+const isPrivacyPage = $body.is('.privacy');
 
 /** Code color variables */
 const $colorSelector = $('.color-selector');
@@ -59,6 +61,10 @@ $(function() {
 
     if (isFaqPage) {
         expandItemOnHashChange();
+    }
+
+    if (isPrivacyPage) {
+        addOrderedListTitleClass();
     }
 });
 
@@ -499,4 +505,17 @@ function copyToClipboard(textToCopy) {
     document.execCommand('copy');
     document.body.removeChild(dummy);
     showSnackbar('Copied to clipboard');
+}
+
+/**
+ * Adds class to the list item that has `h2` tag inside.
+ *
+ * <p>The function is used for the custom ordered list on the Development Support Agreement page.
+ * Styles applies in the `_sass/pages/_privacy.scss` file.
+ */
+function addOrderedListTitleClass() {
+    if ($customOrderedList.length) {
+        const titleListItem = $('.custom-ordered-list h2').parents('li');
+        titleListItem.addClass('title-list-item');
+    }
 }
